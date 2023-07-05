c4f217c61c6f08c170212254b930bf73022b031b
Crowdbotics <team@crowdbotics.com>
2023-07-05 17:34:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vacuum-41925",
+  url: "https://vacuum-41925.botics.co",
+  api: "https://vacuum-41925.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
